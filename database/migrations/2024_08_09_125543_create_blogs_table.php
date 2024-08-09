ded7ece996afa21d330513308f9b89b16a774e45
carljustinm <carlmasedman01@gmail.com>
2024-08-09 20:57:33 +0800

    Defined the schema in the migration file

--- database/migrations/2024_08_09_125543_create_blogs_table.php
+++ database/migrations/2024_08_09_125543_create_blogs_table.php
@@ -9,13 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
+    
 
     /**
      * Reverse the migrations.
