c58e0979e6a62695d05c57c0d405e45ac20a28bb
carljustinm <carlmasedman01@gmail.com>
2024-08-09 21:08:58 +0800

    Configured the Authorization Service Provider

--- app/Policies/BlogPolicy.php
+++ app/Policies/BlogPolicy.php
@@ -3,14 +3,34 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\Blog;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class BlogPolicy
 {
+    use HandlesAuthorization;
+
     /**
-     * Create a new policy instance.
+     * Determine if the given user can update the given blog.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Blog  $blog
+     * @return bool
      */
-    public function __construct()
+    public function update(User $user, Blog $blog)
     {
-        //
+        return $user->id === $blog->user_id;
+    }
+
+    /**
+     * Determine if the given user can delete the given blog.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Blog  $blog
+     * @return bool
+     */
+    public function delete(User $user, Blog $blog)
+    {
+        return $user->id === $blog->user_id;
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         'App\Models\Blog' => 'App\Policies\BlogPolicy',
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap any application services.
+     * Bootstrap services.
      */
     public function boot(): void
     {
